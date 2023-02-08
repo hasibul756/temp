5643e43c1baba1d1ac5d10a4a596a1369e0f4f7e
Hasibul Alam <alamhasibul756@gmail.com>
2023-02-09 00:58:51 +0530

    V1

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+// Names
+define("BANKNAME", "Rural Bank");
+define("EMAIL", "alamhasibul756@gmail.com");
+define("PASSWORD", "qffkjdfxzhnxbong");
+define("ADDRESS", "Kalyani, West Bengal, India 741249");
+define("MOBILENO", "+91 1234567890");
+define("LOCATION_CORDINATE", "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29386.633289146197!2d88.42618586819451!3d22.97492038852493!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f895237d8c2c43%3A0xa617faf57fbd04d1!2sKalyani%2C%20West%20Bengal!5e0!3m2!1sen!2sin!4v1675608024195!5m2!1sen!2sin");
